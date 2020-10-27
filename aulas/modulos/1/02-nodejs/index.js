e158b227f66b08e535d887a29dab9d8f72ee7657
Jonas Braga <jonasbraga2001@gmail.com>
2020-10-26 23:36:23 -0300

    Async callbacks exemples and docs

---
+++ aulas/modulos/1/02-nodejs/index.js
@@ -0,0 +1,58 @@
+/**
+ * Obter um usuário
+ * Obter o número de telefone de um usuário a partir do seu Id
+ * Obter o endereço de um usuário a partir do seu Id
+ */
+
+function obterUsuario(callback) {
+  setTimeout(() => {
+    callback(null, {
+      id: 1,
+      nome: "Jonas",
+      dataNascimento: new Date(),
+    });
+  }, 1000);
+}
+
+function obterTelefone(__idUsuario, callback) {
+  setTimeout(() => {
+    return callback(null, {
+      telefone: "911990022",
+      ddd: 11,
+    });
+  }, 2000);
+}
+
+function obterEndereco(__idUsuario, callback) {
+  setTimeout(() => {
+    return callback(null, {
+      rua: "Principal",
+      numero: 125,
+    });
+  }, 2000);
+}
+
+obterUsuario(function resolverUsuario(error, usuario) {
+  if (error) {
+    console.error("DEU RUIM NO USUARIO", error);
+    return;
+  }
+  obterTelefone(usuario.id, function resolverTelefone(error1, telefone) {
+    if (error1) {
+      console.error("DEU RUIM NO TELEFONE", error);
+      return;
+    }
+    obterEndereco(usuario.id, function resolverEndereco(error2, endereco) {
+      if (error2) {
+        console.error("DEU RUIM NO ENDEREÇO", error);
+        return;
+      }
+
+      console.log(`
+        Nome: ${usuario.nome},
+        Endereço: ${endereco.rua}, ${endereco.numero},
+        Telefone: ${telefone.ddd} - ${telefone.telefone}.
+      `);
+    });
+  });
+});
